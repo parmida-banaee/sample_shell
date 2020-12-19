26cfc7b7f91bd9c0b8b4347c5e5245b625293795
Parmida <banaeeparmida@gamil.com>
2020-12-19 17:30:17 +0330

    working with a while

--- sample_shell.sh
+++ sample_shell.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 #echo "Hello World"
 #Add two numeric value
-((sum=25+35))
+#((sum=25+35))
 #Print the result
-echo $sum
+#echo $sum
+valid=true
+count=1
+while [ $valid ]
+do
+echo $count
+if [ $count -eq 5 ];
+then
+break
+fi
+((++count))
+done
