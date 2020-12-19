66c49e3b11333156540826681237ca0e3dd0c6d6
Parmida <banaeeparmida@gamil.com>
2020-12-19 17:58:22 +0330

    making a function

--- sample_shell.sh
+++ sample_shell.sh
@@ -20,10 +20,16 @@
 #echo -n "$counter "
 #done
 #printf "\n"
-n=10
-if [ $n -lt 10 ];
-then
-echo "It is a one digit number"
-else
-echo "It is a two digit number"
-fi
+#n=10
+#if [ $n -lt 10 ];
+#then
+#echo "It is a one digit number"
+#else
+#echo "It is a two digit number"
+#fi
+function F1()
+{
+echo "I like bash programming"
+}
+
+F1
