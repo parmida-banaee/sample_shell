faac1393e2a71f2028a09487b03bcb78690bbeb9
Parmida <banaeeparmida@gamil.com>
2020-12-19 17:54:35 +0330

    Adding an if

--- sample_shell.sh
+++ sample_shell.sh
@@ -15,8 +15,15 @@
 #fi
 #((++count))
 #done
-for (( counter=10; counter>0; counter-- ))
-do
-echo -n "$counter "
-done
-printf "\n"
+#for (( counter=10; counter>0; counter-- ))
+#do
+#echo -n "$counter "
+#done
+#printf "\n"
+n=10
+if [ $n -lt 10 ];
+then
+echo "It is a one digit number"
+else
+echo "It is a two digit number"
+fi
